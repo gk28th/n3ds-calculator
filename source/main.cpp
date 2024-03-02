9bb00fc7e7138f319b5996fcc340dfa926360c95
gk28th <95129927+gk28th@users.noreply.github.com>
2024-03-02 21:08:17 +0000

    started printing

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,34 @@
-
 #include <3ds.h>
 #include <stdio.h>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
+	// Initialize services
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
 
+	//In this example we need one PrintConsole for each screen
+	PrintConsole topScreen, bottomScreen;
 
-	printf("\x1b[16;20HHello World!");
-	printf("\x1b[30;16HPress Start to exit.");
+	//Initialize console for both screen using the two different PrintConsole we have defined
+	consoleInit(GFX_TOP, &topScreen);
+	consoleInit(GFX_BOTTOM, &bottomScreen);
+
+	//Before doing any text printing we should select the PrintConsole in which we are willing to write, otherwise the library will print on the last selected/initialized one
+	//Let's start by printing something on the top screen
+	consoleSelect(&topScreen);
+	std::cout << "\x1b[15;16HConsole Demo V1.0.0";
+	printf("\x1b[25;16HPress Start to exit!");
+
+	//Now write something else on the bottom screen
+	consoleSelect(&bottomScreen);
+	std::cout <<"Press A for Addition\n\n";
+	std::cout <<"Press B for Subtraction\n\n";
+	std::cout <<"Press X for Multiplication\n\n";
+	std::cout <<"Press Y for Division";
+
+	
+	
 
 	// Main loop
 	while (aptMainLoop())
@@ -30,6 +49,8 @@ int main(int argc, char **argv)
 		gspWaitForVBlank();
 	}
 
+	// Exit services
 	gfxExit();
+
 	return 0;
 }
