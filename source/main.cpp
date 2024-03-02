f5291e6fe939386e9de82676e1411bbb1fd29189
gk28th <95129927+gk28th@users.noreply.github.com>
2024-03-02 20:18:38 +0000

    test

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	// Main loop
 	while (aptMainLoop())
 	{
-		//Scan all the inputs. This should be done once for each fram
+		//Scan all the inputs. This should be done once for each frame
 		hidScanInput();
 
 		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
