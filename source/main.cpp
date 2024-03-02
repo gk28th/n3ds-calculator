73288701b1e69ff8826951c4746963a89ef12c2d
gk28th <95129927+gk28th@users.noreply.github.com>
2024-03-02 19:55:26 +0000

    Add files via upload

---
+++ source/main.cpp
@@ -0,0 +1,35 @@
+
+#include <3ds.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	gfxInitDefault();
+	consoleInit(GFX_TOP, NULL);
+
+
+	printf("\x1b[16;20HHello World!");
+	printf("\x1b[30;16HPress Start to exit.");
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		//Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+
+		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+		u32 kDown = hidKeysDown();
+
+		if (kDown & KEY_START) break; // break in order to return to hbmenu
+
+		// Flush and swap framebuffers
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+
+		//Wait for VBlank
+		gspWaitForVBlank();
+	}
+
+	gfxExit();
+	return 0;
+}
